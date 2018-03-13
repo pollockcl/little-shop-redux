1ed260638f43f548011790d7278aa6cda8297fc5
pollockcl <pollock.clee@gmail.com>
2018-03-13 16:44:28 -0600

    added get paths for the merchants index and merchants/edit pages. also added patch method for merchants/edit page

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,3 +1,20 @@
 class LittleShopApp < Sinatra::Base
+  get '/merchants' do
+    merchants = Merchant.all
+    erb :'merchant/index', locals: { merchants: merchants }
+  end
 
+  get '/merchants/edit/:id' do
+    merchant = Merchant.find(params['id'])
+
+    erb :'merchant/edit', locals: { merchant: merchant }
+  end
+
+  patch '/merchants/edit/:id' do
+    merchant      = Merchant.find(params['id'])
+    merchant.name = params['new_name']
+    merchant.save
+
+    redirect :'/merchants'
+  end
 end
