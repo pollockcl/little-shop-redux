a43afe43b2607f99adcb0155b81407600f2e2a85
pollockcl <pollock.clee@gmail.com>
2018-03-14 17:16:25 -0600

    added attribute validations

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,2 +1,6 @@
 class InvoiceItem < ActiveRecord::Base
+  validates :item_id, presence: true
+  validates :invoice_id, presence: true
+  validates :quantity, presence: true
+  validates :unit_price, presence: true
 end
