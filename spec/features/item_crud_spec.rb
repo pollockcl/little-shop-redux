897d75831b8ca6fe548b2130375ec847401063e7
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-15 17:05:51 -0600

    Adds item CRUD spec

---
+++ spec/features/item_crud_spec.rb
@@ -0,0 +1,56 @@
+RSpec.describe do
+  describe 'When visiting' do
+    describe 'create' do
+      it 'can create item' do
+        Item.create(title: 'Burrito del Greg', description: 'That burrito which belongs to Greg', price: 6, merchant_id: 12337890)
+        visit '/items'
+        
+        expect(current_path).to eq('/items')
+        expect(page).to have_content('Burrito del Greg')
+        expect(Item.count).to eq(1)
+      end
+    end
+
+    describe 'read' do
+      it 'can view item' do
+        Item.create(title: 'Burrito del Greg', description: 'That burrito which belongs to Greg', price: 6, merchant_id: 12337890)
+
+        click_link('Burrito del Greg')
+
+        expect(current_path).to eq('/items/view/1')
+        expect(page).to have_content(6)
+        expect(page).to have_content('Burrito del Greg')
+        expect(page).to have_content('created')
+      end
+    end
+
+    describe 'update' do
+      it 'should edit item' do
+        Item.create(title: 'Burrito del Greg', description: 'That burrito which belongs to Greg', price: 6, merchant_id: 12337890)
+
+        visit 'items/edit/1'
+
+        expect(page).to have_content(6)
+
+        fill_in 'new_title', with: 'Burrito del Gerg'
+        click_button('Submit')
+
+        expect(current_path).to eq('/items')
+        expect(page).to have_content('Burrito del Gerg')
+      end
+    end
+
+    describe 'delete' do
+      it 'should delete a item' do
+        Item.create(title: 'Burrito del Greg')
+        visit '/items'
+
+        expect(current_path).to eq('/items')
+
+        click_button 'Delete'
+
+        expect(page).to_not have_content('Burrito del Greg')
+      end
+    end
+  end
+end
