d4485921ea4bba8ad5b20b0682aa5f322d457e99
pollockcl <pollock.clee@gmail.com>
2018-03-16 09:30:59 -0600

    fixed and changed some of the tests

--- spec/features/item_crud_spec.rb
+++ spec/features/item_crud_spec.rb
@@ -2,20 +2,30 @@ RSpec.describe do
   describe 'When visiting' do
     describe 'create' do
       it 'can create item' do
-        Item.create(title: 'Burrito del Greg', description: 'That burrito which belongs to Greg', price: 6, merchant_id: 12337890)
+        item = Item.create(title: 'Burrito del Greg',
+                           description: 'That burrito which belongs to Greg',
+                           price: 6,
+                           merchant_id: 12337890)
+
         visit '/items'
-        
+
         expect(current_path).to eq('/items')
-        expect(page).to have_content('Burrito del Greg')
+        expect(page).to have_content(item.title)
         expect(Item.count).to eq(1)
       end
     end
 
     describe 'read' do
       it 'can view item' do
-        Item.create(title: 'Burrito del Greg', description: 'That burrito which belongs to Greg', price: 6, merchant_id: 12337890)
+        Item.create(title: 'Burrito del Greg',
+                    description: 'That burrito which belongs to Greg',
+                    price: 6,
+                    merchant_id: 12337890)
 
-        click_link('Burrito del Greg')
+        visit '/items'
+
+        click_link ('Burrito del Greg')
+
 
         expect(current_path).to eq('/items/view/1')
         expect(page).to have_content(6)
@@ -28,26 +38,28 @@ RSpec.describe do
       it 'should edit item' do
         Item.create(title: 'Burrito del Greg', description: 'That burrito which belongs to Greg', price: 6, merchant_id: 12337890)
 
-        visit 'items/edit/1'
+        visit '/items/edit/1'
 
-        expect(page).to have_content(6)
+        expect(page).to have_field('new_title', with: 'Burrito del Greg')
 
         fill_in 'new_title', with: 'Burrito del Gerg'
         click_button('Submit')
 
-        expect(current_path).to eq('/items')
+        expect(current_path).to eq('/items/edit/1')
         expect(page).to have_content('Burrito del Gerg')
       end
     end
 
     describe 'delete' do
       it 'should delete a item' do
-        Item.create(title: 'Burrito del Greg')
+        Item.create(title: 'Burrito del Greg',
+                    description: 'That burrito which belongs to Greg',
+                    price: 6,
+                    merchant_id: 12337890)
+
         visit '/items'
 
-        expect(current_path).to eq('/items')
-
-        click_button 'Delete'
+        click_on 'Delete'
 
         expect(page).to_not have_content('Burrito del Greg')
       end
