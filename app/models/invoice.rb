3a80b7c346a302b4b7fc02ae8d2596459544e18a
jrambold <jrambold@gmail.com>
2018-03-18 16:54:47 -0600

    setup invoice methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,31 @@
 class Invoice < ActiveRecord::Base
   validates :merchant_id, presence: true
+
+  def self.pending
+
+  end
+
+  def self.shipped
+
+  end
+
+  def self.returned
+
+  end
+
+  def self.highest_price
+
+  end
+
+  def self.lowest_price
+
+  end
+
+  def highest_quantity
+
+  end
+
+  def lowest_quantity
+
+  end
 end
