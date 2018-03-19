ad48eb7879fb8bf1b3d1d29eb870e40c15cc3ed5
jrambold <jrambold@gmail.com>
2018-03-19 05:45:45 -0600

    fixed merchant dbd and methods. still need total

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,12 +3,15 @@ class Merchant < ActiveRecord::Base
   has_many :items
 
   def self.most_items
-    all.max_by(&:item_count)
+    all.max_by do |merchant|
+      merchant.items.count
+    end
   end
 
-  def self.high_price
-    high = Item.maximum('merchant_id')
-    Item.where(price: high).to_a
+  def self.highest_price
+    high = Item.maximum('price')
+    highest = (Item.where(price: high).to_a).map(&:merchant_id)
+    highest.map { |m_id| Merchant.find(m_id) }
   end
 
   def item_count
