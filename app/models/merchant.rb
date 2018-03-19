e26c9ef353425f5235fa40381364105765c925d9
jrambold <jrambold@gmail.com>
2018-03-18 22:31:11 -0600

    merchant methods added

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,19 +3,21 @@ class Merchant < ActiveRecord::Base
   has_many :items
 
   def self.most_items
-
+    all.max_by(&:item_count)
   end
 
   def self.high_price
-
+    high = Item.maximum('merchant_id')
+    Item.where(price: high).to_a
   end
 
   def item_count
-
+    Item.where(merchant_id: id).to_a.count
   end
 
   def avg_price
-
+    items = Item.where(merchant_id: id).to_a
+    items.map(&:price).sum / items.count
   end
 
   def total_cost
