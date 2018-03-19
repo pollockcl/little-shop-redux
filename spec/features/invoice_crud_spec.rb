e8e834ef109b54d3b6bde0dd40f26c474fda9a1f
pollockcl <pollock.clee@gmail.com>
2018-03-19 12:11:50 -0600

    fixed broken tests

--- spec/features/invoice_crud_spec.rb
+++ spec/features/invoice_crud_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe Invoice do
   describe 'When visiting' do
+    before(:each) do
+      Invoice.create(merchant_id: '83770')
+    end
 
     describe 'read' do
       it 'can view invoice' do
-        Invoice.create(merchant_id: '83770')
         visit '/invoices'
 
         click_link('1')
@@ -16,12 +18,9 @@ RSpec.describe Invoice do
 
     describe 'update' do
       it 'should edit invoice' do
-        Invoice.create(merchant_id: '83770')
         visit '/invoices'
 
         visit 'invoices/1/edit'
-
-        expect(page).to have_content(1)
 
         fill_in 'new_customer_id', with: '1337'
         fill_in 'new_merchant_id', with: '8008'
@@ -29,6 +28,8 @@ RSpec.describe Invoice do
         click_button('Submit')
 
         expect(current_path).to eq('/invoices')
+
+        visit '/invoices/1/view'
         expect(page).to have_content('1337')
         expect(page).to have_content('8008')
         expect(page).to have_content('High Ground')
@@ -37,14 +38,13 @@ RSpec.describe Invoice do
 
     describe 'delete' do
       it 'should delete a invoice' do
-        Invoice.create(merchant_id: '1337')
         visit '/invoices'
 
         expect(current_path).to eq('/invoices')
 
         click_button 'Delete'
 
-        expect(page).to_not have_content('1337')
+        expect(page).to_not have_content('83770')
       end
     end
   end
