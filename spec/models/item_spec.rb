9d271c70d2f3015eddbbc8a6b3364ae56ab5e0e0
pollockcl <pollock.clee@gmail.com>
2018-03-20 12:56:12 -0600

    format is slightly more consistent

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,9 @@ RSpec.describe Item do
   describe 'Validates attributes' do
     describe '.name' do
       it 'should be present' do
-        item = Item.create(description: 'Glorious adventure', price: 131, merchant_id: 66)
+        item = Item.create(description: 'Glorious adventure',
+                           price: 131,
+                           merchant_id: 66)
 
         expect(item).to_not be_valid
       end
@@ -11,7 +13,9 @@ RSpec.describe Item do
 
     describe '.description' do
       it 'should be present' do
-        item = Item.create(title: 'Bilbo', price: 131, merchant_id: 66)
+        item = Item.create(title: 'Bilbo',
+                           price: 131,
+                           merchant_id: 66)
 
         expect(item).to_not be_valid
       end
@@ -19,7 +23,9 @@ RSpec.describe Item do
 
     describe '.unit_price' do
       it 'should be present' do
-        item = Item.create(title: 'Bilbo', description: 'Glorious adventure', merchant_id: 66)
+        item = Item.create(title: 'Bilbo',
+                           description: 'Glorious adventure',
+                           merchant_id: 66)
 
         expect(item).to_not be_valid
       end
@@ -27,7 +33,9 @@ RSpec.describe Item do
 
     describe '.merchant_id' do
       it 'should be present' do
-        item = Item.create(title: 'Bilbo', description: 'Glorious adventure', price: 131)
+        item = Item.create(title: 'Bilbo',
+                           description: 'Glorious adventure',
+                           price: 131)
 
         expect(item).to_not be_valid
       end
