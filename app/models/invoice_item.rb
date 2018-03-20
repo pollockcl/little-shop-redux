5a5f35a962b95ec5670644b4ae516c9ad037ac1d
pollockcl <pollock.clee@gmail.com>
2018-03-20 11:20:47 -0600

    got rid of unused class method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,21 +5,4 @@ class InvoiceItem < ActiveRecord::Base
   validates :unit_price, presence: true
   belongs_to :invoice
   belongs_to :item
-
-
-  def self.invoice_view(invoice_id)
-    ii = where(invoice_id: invoice_id).to_a
-
-    items = ii.map do |x|
-      Item.find(x.item_id)
-    end
-
-    c = ii.zip(items).to_h
-
-    receipts = c.map{ |k, v| { quantity: k[:quantity], price: v[:price] } }
-    total = c.map{ |k,v| k[:quantity] * v[:price] }.sum
-
-
-    { total: total, receipts: receipts }
-  end
 end
